7cd9c64aa029deab6ef34ea678e78e8f5b67390d
Leftas <leftaslt@gmail.com>
2020-12-31 01:15:47 +0200

    Add decoding for LZ78

--- LZ78/main.py
+++ LZ78/main.py
@@ -1,18 +1,44 @@
 ''''LZ78 algorithm'''
 
+from math import log2
+
+from bitarray import bitarray
+from bitarray.util import ba2int
+
+
 # 5 PIRMI BITAI - K (0 - 31) (0 - inf)
+
 
 def encode(dict_size: int):
     pass
 
-def decode():
+
+def decode(file: str):
     dict_size = 0
-    dictionary = {}
+    dictionary = [bytearray()]
+    output_buffer = bytearray()
     with open(file, mode='rb') as f:
-        buffer_len = len(input_buffer)
-        
-        pass
+        input_buffer = bitarray()
+        input_buffer.fromfile(f)
+        k = ba2int(input_buffer[0:4])
+        n = 2 ** k
+        while len(input_buffer) > 0:
+            index_len = int(log2(dict_size if dict_size > 0 else 1))
+
+            index = ba2int(input_buffer[:index_len])
+            word = input_buffer[index_len:index_len + 8]
+
+            entry = bytearray()
+            entry.extend(dictionary[index])
+            entry.append(word)
+
+            if len(dictionary) < n:
+                dictionary.append(entry)
+            output_buffer.extend(entry)
+
+            input_buffer = input_buffer[index_len + 8:]
     pass
+
 
 if __name__ == "__main__":
     pass
