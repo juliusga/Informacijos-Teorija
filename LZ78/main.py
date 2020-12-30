24a25a3594608a741d0f7afc627e068a24f941fc
unknown <julius.gaidukevicius@mif.stud.vu.lt>
2020-12-30 21:24:40 +0200

    Comments added

---
+++ LZ78/main.py
@@ -0,0 +1,18 @@
+''''LZ78 algorithm'''
+
+# 5 PIRMI BITAI - K (0 - 31) (0 - inf)
+
+def encode(dict_size: int):
+    pass
+
+def decode():
+    dict_size = 0
+    dictionary = {}
+    with open(file, mode='rb') as f:
+        buffer_len = len(input_buffer)
+        
+        pass
+    pass
+
+if __name__ == "__main__":
+    pass
