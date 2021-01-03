32241454f3d1bb5766bf273b17dc10f517c7dd5b
Leftas <leftaslt@gmail.com>
2021-01-03 22:03:35 +0200

    Add argument parsing for LZ78

--- LZ78/main.py
+++ LZ78/main.py
@@ -1,11 +1,11 @@
 ''''LZ78 algorithm'''
 
 from math import log2, inf
-from pathlib import Path
 
 from bitarray import bitarray
 from bitarray.util import ba2int, int2ba
 
+import argparse
 
 # 5 PIRMI BITAI - K (0 - 31) (0 - inf)
 # K | Dictionary size
@@ -16,10 +16,9 @@ from bitarray.util import ba2int, int2ba
 # ...
 
 
-def encode(dict_size: int, file: str):
-    with open(file, mode='rb') as input_file:
-        input_buffer = bitarray()
-        input_buffer.fromfile(input_file)
+def encode(dict_size: int, in_file, out_file):
+    input_buffer = bitarray()
+    input_buffer.fromfile(in_file)
 
     dictionary = []
     output_buffer = bitarray()
@@ -53,71 +52,62 @@ def encode(dict_size: int, file: str):
         # Shrink the input buffer
         input_buffer = input_buffer[bits_read:] if entry_symbol is not None else bitarray()
 
-    with open(file + '.compressed' + str(dict_size), mode='wb') as out:
-        output_buffer.fill()
-        out.write(output_buffer)
-        print("FILE <" + file + "> COMPRESSED TO <" + file + '.compressed' + str(dict_size) + ">")
+    output_buffer.fill()
+    out_file.write(output_buffer)
+    print("FILE <" + in_file.name + "> COMPRESSED TO <" + out_file.name + ">")
 
 
-def decode(file: str):
+def decode(in_file, out_file):
     entry_index = 1
     dictionary = [bytearray()]
     output_buffer = bytearray()
-    with open(file, mode='rb') as f:
-        input_buffer = bitarray()
-        input_buffer.fromfile(f)
-        k = ba2int(input_buffer[:5])
-        n = 2 ** k if k > 0 else inf
-        input_buffer = input_buffer[5:]
-        input_len = len(input_buffer)
-        current_index = 0
-        while input_len - current_index > 0:
-            index_len = int(log2(entry_index - 1 if entry_index > 1 else 1)) + 1  # 1 - 0, 2 - 0,1, 3-0,1,2, 4-0,1,2,3.
-            # 1,2 - 1 bit, 3,4 - 2 bits, 5,6,7,8-3 bits, 9,10,11,12,13,14,15,16 - 4 bits
+    input_buffer = bitarray()
+    input_buffer.fromfile(in_file)
+    k = ba2int(input_buffer[:5])
+    n = 2 ** k if k > 0 else inf
+    input_buffer = input_buffer[5:]
+    input_len = len(input_buffer)
+    current_index = 0
+    while input_len - current_index > 0:
+        index_len = int(log2(entry_index - 1 if entry_index > 1 else 1)) + 1  # 1 - 0, 2 - 0,1, 3-0,1,2, 4-0,1,2,3.
+        # 1,2 - 1 bit, 3,4 - 2 bits, 5,6,7,8-3 bits, 9,10,11,12,13,14,15,16 - 4 bits
 
-            index = ba2int(input_buffer[current_index:current_index + index_len])
+        index = ba2int(input_buffer[current_index:current_index + index_len])
 
-            entry = bytearray()
-            entry.extend(dictionary[index])
+        entry = bytearray()
+        entry.extend(dictionary[index])
 
-            if (input_len - current_index) - index_len > 8:
-                word = input_buffer[current_index + index_len:current_index + index_len + 8]
-                entry.append(ba2int(word))
-                current_index = current_index + index_len + 8
-                entry_index = entry_index + 1
-            else:
-                current_index = input_len
+        if (input_len - current_index) - index_len > 8:
+            word = input_buffer[current_index + index_len:current_index + index_len + 8]
+            entry.append(ba2int(word))
+            current_index = current_index + index_len + 8
+            entry_index = entry_index + 1
+        else:
+            current_index = input_len
 
-            if n is inf or len(dictionary) <= n:
-                dictionary.append(entry)
-            output_buffer.extend(entry)
+        if n is inf or len(dictionary) <= n:
+            dictionary.append(entry)
+        output_buffer.extend(entry)
 
-    file_path = Path(file)
-
-    if file_path.suffix.startswith('.compressed'):
-        out_filename = file_path.stem + '.uncompressed'
-        with open(out_filename, mode='wb') as out:
-            out.write(output_buffer)
-            print(f"FILE <{file}> DECOMPRESSED TO <{out_filename}>")
+    out_file.write(output_buffer)
+    print(f"FILE <{in_file.name}> DECOMPRESSED TO <{out_file.name}>")
 
 
 if __name__ == "__main__":
-    print('\n')
-    # encode(0, 'test_text.txt')
-    # encode(0, 'test_text_longer.txt')
-    # encode(0, 'test_image.bmp')
-    # encode(1, 'test_text.txt')
-    # encode(1, 'test_text_longer.txt')
-    # encode(1, 'test_image.bmp')
-    # encode(2, 'test_text.txt')
-    # encode(2, 'test_text_longer.txt')
-    # encode(2, 'test_image.bmp')
-    # decode('test_text.txt.compressed0')
-    # decode('test_text.txt.compressed1')
-    # decode('test_text.txt.compressed2')
-    # decode('test_text_longer.txt.compressed0')
-    # decode('test_text_longer.txt.compressed1')
-    # decode('test_text_longer.txt.compressed2')
-    # decode('test_image.bmp.compressed0')
-    # decode('test_image.bmp.compressed1')
-    # decode('test_image.bmp.compressed2')
+    parser = argparse.ArgumentParser(description='(De-)Compression using LZ78 algorithm.')
+    parser.add_argument('-k', type=int, default=16, choices=range(0, 32),
+                        help='Number raised by power of 2 for defining dictionary size')
+    parser.add_argument('--type', choices=['encode', 'decode', 'e', 'd'], type=str, required=True,
+                        help='Type of operation')
+    parser.add_argument('infile', nargs=1, type=argparse.FileType('rb'),
+                        help='File to (de-)compress')
+    parser.add_argument('outfile', nargs=1, type=argparse.FileType('wb'),
+                        help='Output file for (de-)compression')
+
+    args = parser.parse_args()
+
+    if args.type == 'e' or args.type == 'encode':
+        encode(args.k, args.infile[0], args.outfile[0])
+    elif args.type == 'd' or args.type == 'decode':
+        decode(args.infile[0], args.outfile[0])
+
